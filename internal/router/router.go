19bfaa6e227e45a1e2968901c403355417fb997f
anatolyi <anatolyi030311@gmail.com>
2025-10-14 03:54:31 +0300

    fix checks

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,26 +2,28 @@ package router
 
 import (
 	"io"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
 	"sync"
+	"time"
 )
 
 // curl -X POST -H "Content-Type: text/plain" -d 'https://practicum.yandex.ru/' http://localhost:8080/
 // curl -X GET -H "Content-Type: text/plain" -d '/EwHXdJfB' http://localhost:8080/{id}
 
-// const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // "0123456789"
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // "0123456789"
 
-// func generateRandomString(length int) string {
-// 	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
-// 	var result []byte
-// 	for i := 0; i < length; i++ {
-// 		index := seededRand.Intn(len(charset))
-// 		result = append(result, charset[index])
-// 	}
-// 	return string(result)
-// }
+func generateRandomString(length int) string {
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var result []byte
+	for i := 0; i < length; i++ {
+		index := seededRand.Intn(len(charset))
+		result = append(result, charset[index])
+	}
+	return string(result)
+}
 
 // func GetOriginURL(key string) string {
 // 	urls := map[string]string{
@@ -71,7 +73,7 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	name := string(body)[1:] + ".txt"
+	name := string(body) + ".txt"
 	data, err := os.ReadFile(name)
 	if err != nil {
 		panic(err)
@@ -86,8 +88,12 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	modelURL := NewModelURL()
 	if r.Method != http.MethodPost {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "text/plain" {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
@@ -109,15 +115,13 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	// data := make([]byte, 0)
-	name := string(newURL.Path)[1:] + ".txt"
-	if string(newURL.Path) == "/" {
-		name = ".txt"
-	}
-	err = os.WriteFile(name, append(body, []byte(newURL.String())...), os.ModePerm)
+	name := generateRandomString(6) + ".txt"
+	err = os.WriteFile(name, []byte(newURL.String()), os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
-	modelURL.SetURL(newURL, parseURL.Path)
+	// modelURL := NewModelURL()
+	// modelURL.SetURL(newURL, parseURL.Path)
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(newURL.String()))
 }
