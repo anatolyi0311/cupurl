b4012c36af093a3b932d9a7d93cb3c07374992d3
anatolyi <anatolyi030311@gmail.com>
2025-10-19 17:22:05 +0300

    fix iter4

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,23 +16,37 @@ type Options struct {
 	BaseURL string
 }
 
+func ParseURL(addr *string, param string) error {
+	_, err := url.Parse("https://" + *addr)
+	if err != nil {
+		return fmt.Errorf("incorrect parametr `-%s` %s", param, *addr)
+	}
+	return nil
+}
+
 func newOpts() (*Options, error) {
 	var addr = flag.String("a", "localhost:8080", "server host")
 	var baseURL = flag.String("b", "localhost:8080", "host before short URL")
 	flag.Parse()
 
-	if _, err := url.Parse("https://" + *addr); err != nil {
-		return nil, fmt.Errorf("incorrect arg `-a` %s", *addr)
+	if ParseURL(addr, "a") != nil {
+		return nil, ParseURL(addr, "a")
+	}
+	if ParseURL(baseURL, "b") != nil {
+		return nil, ParseURL(baseURL, "b")
 	}
 
-	if _, err := url.Parse("https://" + *baseURL); err != nil {
-		return nil, fmt.Errorf("incorrect arg `-b` %s", *baseURL)
+	if !strings.HasPrefix(*addr, "http://") && !strings.HasPrefix(*addr, "https://") {
+		*baseURL = "http://" + *addr
 	}
+	*addr = strings.TrimSuffix(*addr, "/")
+	*baseURL = strings.TrimSuffix(*baseURL, "/")
 
 	if !strings.HasPrefix(*baseURL, "http://") && !strings.HasPrefix(*baseURL, "https://") {
 		*baseURL = "http://" + *baseURL
 	}
 	*baseURL = strings.TrimSuffix(*baseURL, "/")
+
 	return &Options{
 		Addr:    *addr,
 		BaseURL: *baseURL,
