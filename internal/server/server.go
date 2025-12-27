f9c21936cbbebd90d2679578f7672e919be68936
dj-dev <77513044+anatolyi0311@users.noreply.github.com>
2025-12-27 19:40:53 +0300

    Update server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -297,11 +297,11 @@ func (s *Server) GetArrayURLJson(res http.ResponseWriter, req *http.Request) {
 
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			http.Error(res, err.Error(), http.StatusNoContent)
+			http.Error(res, err.Error(), http.StatusGone)
 			return
 		}
 		s.logger.Errorln(err)
-		http.Error(res, err.Error(), http.StatusBadRequest)
+		http.Error(res, err.Error(), http.StatusGone)
 		return
 	}
 
