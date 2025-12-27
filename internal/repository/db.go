64bc3a82a1efb85a2cb3b6eb1c8541410a44f63e
anatolyi <anatolyi030311@gmail.com>
2025-12-28 01:34:46 +0300

    fix 1:1

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -99,7 +99,7 @@ func (s *Storage) DeleteDB(hash string, userID int, logger zap.SugaredLogger) er
 	err := s.db.QueryRow(`SELECT originalURL, deletedFlag FROM cupurl WHERE shortURL = $1 AND deletedFlag = true;`, hash).Scan(&originalURL, &isDeleted)
 	if err != nil {
 		logger.Warnf("%s", err)
-		return fmt.Errorf("%s", sql.ErrNoRows)
+		return nil // fmt.Errorf("%s", sql.ErrNoRows)
 	}
 
 	result, err := s.db.Exec(`
