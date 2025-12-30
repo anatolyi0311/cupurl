fb1bb077d295c093083394b8eb174c771d80f22b
anatolyi <anatolyi030311@gmail.com>
2025-12-31 00:50:48 +0300

    fix iter15

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"context"
 	"database/sql"
 	"encoding/json"
 	"errors"
@@ -359,8 +358,8 @@ func (s *Server) DeleteArrayURLJson(res http.ResponseWriter, req *http.Request) 
 	s.cfg.Opts.User = userID
 	s.logger.Info("DeleteArrayURLJson.userID: ", userID, "...", s.cfg.Opts.User)
 
-	// s.su.DeleteArrayURL(hashArray, userID)
-	s.su.DeleteUrls(context.Background(), hashArray, userID)
+	s.su.DeleteArrayURL(hashArray, userID)
+	// s.su.DeleteUrls(context.Background(), hashArray, userID)
 
 	res.WriteHeader(http.StatusAccepted)
 }
