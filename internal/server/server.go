1b732f563d650fa129e4f233a47e259c2dd8a1cb
anatolyi <anatolyi030311@gmail.com>
2026-02-02 02:44:18 +0300

    fix 15

--- internal/server/server.go
+++ internal/server/server.go
@@ -305,8 +305,8 @@ func (s *Server) GetArrayURLJson(res http.ResponseWriter, req *http.Request) {
 	userID, err := jwt.GetUserID(req)
 	if err != nil {
 		s.logger.Warn("not user ", err.Error())
-		// http.Error(res, err.Error(), http.StatusNoContent)
-		// return
+		http.Error(res, err.Error(), http.StatusNoContent)
+		return
 	}
 	s.cfg.Opts.User = userID
 	// s.logger.Info("GetArrayURLJson.userID: ", userID)
