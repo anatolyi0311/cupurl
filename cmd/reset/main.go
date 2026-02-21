cf4487144001992e4935d775dc366e21fa6983ab
anatolyi <anatolyi030311@gmail.com>
2026-02-21 17:29:12 +0300

    fix iter21 garbage

--- cmd/reset/main.go
+++ cmd/reset/main.go
@@ -2,47 +2,99 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"go/ast"
 	"go/parser"
 	"go/printer"
 	"go/token"
-	"log"
-	"os"
 )
 
+type StructInfo struct {
+	Name   string
+	Target *ast.GenDecl
+}
+
 func main() {
-
-	dir, err := os.Getwd()
+	curentDir, err := os.Getwd()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println("dir:", dir)
+	splitDir := strings.Split(curentDir, string(os.PathSeparator))
+	rootDir := filepath.Join(splitDir[:len(splitDir)-2]...)
+	dirKey := string(os.PathSeparator) + splitDir[len(splitDir)-3]
+	rootDir = string(os.PathSeparator) + rootDir
+	// fmt.Println("rootdir:", rootDir)
 
-	homeDir, err := os.UserHomeDir()
+	givenFiles, err := readFileDir(rootDir, dirKey, "", make(map[string][]string))
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println("homeDir:", homeDir)
+	for filePath, files := range givenFiles {
+		// fmt.Println("files:", filePath, "\t", files)
+		// создаём token.FileSet
+		for _, file := range files {
+			fset := token.NewFileSet()
+			// текст исходного кода
+			// src := readFile(filePath)
+			filePath := rootDir[:len(rootDir)-1-len("cupurl")] + filePath + "/" + file
+			// получаем дерево разбора
+			f, err := parser.ParseFile(fset, filePath, nil, parser.ParseComments)
+			if err != nil {
+				fmt.Println(err)
+			}
+			for _, f := range f.Decls {
+				genD, ok := f.(*ast.GenDecl)
+				if !ok {
+					fmt.Printf("SKIP %T is not *ast.GenDecl\n", f)
+					continue
+				}
+				targetStruct := &StructInfo{}
+				var thisIsStruct bool
+				for _, spec := range genD.Specs {
+					currType, ok := spec.(*ast.TypeSpec)
+					if !ok {
+						fmt.Printf("SKIP %T is not ast.TypeSpec\n", spec)
+						continue
+					}
 
-	// absolute path to file for inspect.
-	root := homeDir + "/yapracticum/cupurl/"
-	path := []string{
-		"cmd/shortener/main.go",
-		"internal/app/auth/auth.go",
-		"internal/app/handlers/handlers.go",
-	}
-	filePath := root + path[len(path)-1]
+					currStruct, ok := currType.Type.(*ast.StructType)
+					if !ok {
+						fmt.Printf("SKIP %T is not ast.StructType\n", currStruct)
+						continue
+					}
+					targetStruct.Name = currType.Name.Name
+					thisIsStruct = true
+				}
+				//Getting comments
+				var needCodegen bool
+				var dbeParams string
+				if thisIsStruct {
+					for _, comment := range genD.Doc.List {
+						needCodegen = needCodegen || strings.HasPrefix(comment.Text, "// dbe")
+						if len(comment.Text) < 7 {
+							dbeParams = ""
+						} else {
+							dbeParams = strings.Replace(comment.Text, "// dbe:", "", 1)
+						}
+					}
+				}
+				fmt.Println("dbeParams", dbeParams)
+			}
 
-	// создаём token.FileSet
-	fset := token.NewFileSet()
-	// текст исходного кода
-	src := readFile(filePath)
-	// получаем дерево разбора
-	f, err := parser.ParseFile(fset, "", src, parser.AllErrors)
-	if err != nil {
-		fmt.Println(err)
+			// for _, gr := range f.Comments {
+			// 	for _, c := range gr.List {
+			// 		if strings.HasPrefix(c.Text, "// generate:reset") {
+			// 			fmt.Println(fset.Position(c.Slash).String(), c.Text)
+			// 			inspect(f, fset)
+			// 		}
+			// 	}
+			// }
+		}
 	}
-	inspect(f, fset)
 }
 
 func inspect(node ast.Node, token *token.FileSet) {
@@ -62,21 +114,21 @@ func inspect(node ast.Node, token *token.FileSet) {
 		// 	fmt.Printf("FuncDecl %s %v: ", x.Name.Name, token.Position(x.Pos()))
 		// 	printer.Fprint(os.Stdout, token, x)
 		// 	fmt.Println()
-		case *ast.TypeSpec:
-			// ast.TypeSpec представляет декларацию ...
-			fmt.Printf("TypeSpec\nName: %v pos:%v ...: %v %v\n", x.Name.String(), token.Position(x.Pos()), x.Name.Obj.Kind, x.Name.Obj.Name)
-			// fmt.Printf("%v ", x.Name.Obj.Kind)
-			printer.Fprint(os.Stdout, token, x)
-			fmt.Println()
+		// case *ast.TypeSpec:
+		// 	// ast.TypeSpec представляет декларацию ...
+		// 	fmt.Printf("TypeSpec\nName: %v pos:%v ...: %v %v\n", x.Name.String(), token.Position(x.Pos()), x.Name.Obj.Kind, x.Name.Obj.Name)
+		// 	// fmt.Printf("%v ", x.Name.Obj.Kind)
+		// 	printer.Fprint(os.Stdout, token, x)
+		// 	fmt.Println()
 		case *ast.StructType:
 			// ast.StructType представляет декларацию struct
-			fmt.Printf("StructType\nStruct: %v pos:%v fields: %d\n%v\n", x.Struct, token.Position(x.Pos()), len(x.Fields.List), x)
+			fmt.Printf("StructType\npos:%v fields: %d\n", token.Position(x.Pos()), len(x.Fields.List))
 			printer.Fprint(os.Stdout, token, x)
 			fmt.Println()
-		case *ast.Comment:
-			fmt.Printf("CommentGroup\n%v\n", x.Slash)
-			printer.Fprint(os.Stdout, token, x)
-			fmt.Println()
+			// case *ast.Comment:
+			// 	fmt.Printf("CommentGroup\n%v\n", x.Slash)
+			// 	printer.Fprint(os.Stdout, token, x)
+			// 	fmt.Println()
 		}
 		return true
 	})
@@ -91,4 +143,64 @@ func readFile(filePath string) string {
 	// Print the content (convert byte slice to string)
 	// fmt.Println(string(content))
 	return string(content)
+}
+
+func readFileDir(rootDir, keyDir, sep string, givenFiles map[string][]string) (map[string][]string, error) {
+	/*
+		err := filepath.Walk(".",
+			func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			fmt.Println(path, info.Size())
+			return nil
+		})
+		if err != nil {
+			log.Println(err)
+		}
+	*/
+	files, err := os.ReadDir(rootDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		info, err := file.Info()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if file.Name() == "Makefile" ||
+			file.Name() == "profiles" ||
+			file.Name() == ".git" ||
+			file.Name() == ".github" ||
+			file.Name() == ".idea" ||
+			strings.HasPrefix(file.Name(), ".git") ||
+			strings.Contains(file.Name(), "_mock") ||
+			strings.Contains(file.Name(), "_test") ||
+			strings.HasSuffix(info.Name(), ".md") ||
+			strings.HasSuffix(file.Name(), ".mod") ||
+			strings.HasSuffix(file.Name(), ".sum") ||
+			strings.HasSuffix(file.Name(), ".conf") ||
+			file.Name() == "mocks" {
+			continue
+		}
+		if file.IsDir() {
+			// if sep == "" {
+			// 	fmt.Println(file.Name())
+			// } else {
+			// 	fmt.Println(sep, file.Name())
+			// }
+			subDir := rootDir + string(os.PathSeparator) + file.Name()
+			keyDir := keyDir + string(os.PathSeparator) + file.Name()
+			readFileDir(subDir, keyDir, sep+"    ", givenFiles)
+		}
+		if !file.IsDir() {
+			// if sep == "" {
+			// 	fmt.Println("file: ", file.Name())
+			// } else {
+			// 	fmt.Println(sep, "file: ", file.Name())
+			// }
+			givenFiles[keyDir] = append(givenFiles[keyDir], file.Name())
+		}
+	}
+	return givenFiles, nil
 }
