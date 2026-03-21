123e0759ba3306d5c00edb061bc2d3e69c721346
anatolyi <anatolyi030311@gmail.com>
2026-03-21 20:38:51 +0300

    fix iter28 tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"bytes"
+	"errors"
 	"flag"
+	"fmt"
 	"os"
 	"testing"
 
@@ -14,6 +17,7 @@ func TestNewConfig(t *testing.T) {
 		envVars        map[string]string
 		flagArgs       []string
 		expectedConfig *ENVConfig
+		expectedError  error
 	}{
 		{
 			name: "default values",
@@ -23,6 +27,7 @@ func TestNewConfig(t *testing.T) {
 				EnvStoragePath: "/tmp/short-url-db.json",
 				EnvLogLevel:    "info",
 				EnvDataBase:    "",
+				EnvGRPC:        ":3200",
 			},
 		},
 		{
@@ -33,6 +38,9 @@ func TestNewConfig(t *testing.T) {
 				"FILE_STORAGE_PATH": "/tmp/test.json",
 				"LOG_LEVEL":         "debug",
 				"DATABASE_DSN":      "environment",
+				"ENABLE_TLS":        "disable",
+				"TRUSTED_SUBNET":    "1.1.1.1",
+				"GRPC_SERVER":       ":3000",
 			},
 			flagArgs: []string{},
 			expectedConfig: &ENVConfig{
@@ -41,6 +49,9 @@ func TestNewConfig(t *testing.T) {
 				EnvStoragePath: "/tmp/test.json",
 				EnvLogLevel:    "debug",
 				EnvDataBase:    "environment",
+				EnvTLS:         "disable",
+				EnvSubnet:      "1.1.1.1",
+				EnvGRPC:        ":3000",
 			},
 		},
 		{
@@ -51,6 +62,9 @@ func TestNewConfig(t *testing.T) {
 				"-f", "/tmp/flag-test.json",
 				"-l", "error",
 				"-d", "flags",
+				"-ts", "enable",
+				"-t", "2.2.2.2",
+				"-g", ":1000",
 			},
 			expectedConfig: &ENVConfig{
 				EnvServAdr:     "localhost:7070",
@@ -58,7 +72,30 @@ func TestNewConfig(t *testing.T) {
 				EnvStoragePath: "/tmp/flag-test.json",
 				EnvLogLevel:    "error",
 				EnvDataBase:    "flags",
+				EnvTLS:         "enable",
+				EnvSubnet:      "2.2.2.2",
+				EnvGRPC:        ":1000",
 			},
+		},
+		{
+			name: "flag -c error find file",
+			flagArgs: []string{
+				"-c", "/c",
+				"-b", "http://localhost:7070",
+				"-f", "/tmp/flag-test.json",
+				"-l", "error",
+				"-d", "flags",
+			},
+			expectedConfig: nil,
+			expectedError:  errors.New("open /c: no such file or directory"),
+		},
+		{
+			name: "env CONFIG -c error find file",
+			envVars: map[string]string{
+				"CONFIG": "/c",
+			},
+			expectedConfig: nil,
+			expectedError:  errors.New("open /c: no such file or directory"),
 		},
 	}
 
@@ -83,10 +120,56 @@ func TestNewConfig(t *testing.T) {
 			os.Args = append([]string{"cmd"}, tt.flagArgs...)
 
 			// Call the function under test
-			gotConfig := NewConfig()
-
+			gotConfig, err := NewConfig()
+			if err != nil {
+				assert.EqualError(t, tt.expectedError, err.Error())
+			}
 			// Assert the result
 			assert.Equal(t, tt.expectedConfig, gotConfig)
+
 		})
+	}
+}
+
+func TestGetValueOrDefault(t *testing.T) {
+	// Test case 1: value is not empty
+	result := getValueOrDefault("testValue", "defaultValue")
+	expected := "testValue"
+	if result != expected {
+		t.Errorf("Expected: %s, Got: %s", expected, result)
+	}
+
+	// Test case 2: value is empty
+	result = getValueOrDefault("", "defaultValue")
+	expected = "defaultValue"
+	if result != expected {
+		t.Errorf("Expected: %s, Got: %s", expected, result)
+	}
+}
+
+func TestPrintProjectInfo(t *testing.T) {
+	// Redirect stdout to capture printed output
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	// Run the PrintProjectInfo function
+	PrintProjectInfo()
+
+	// Read captured output
+	var buf bytes.Buffer
+	w.Close()
+	buf.ReadFrom(r)
+	os.Stdout = old
+
+	// Expected output
+	expectedOutput := fmt.Sprintf("Build version: %s\nBuild date: %s\nBuild commit: %s\n",
+		getValueOrDefault(buildVersion, "2.0"),
+		getValueOrDefault(buildDate, "13.04.2024"),
+		getValueOrDefault(buildCommit, "Clean architect"))
+
+	// Check if the printed output matches the expected output
+	if buf.String() != expectedOutput {
+		t.Errorf("Expected output: %s, Got: %s", expectedOutput, buf.String())
 	}
 }
